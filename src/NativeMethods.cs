5165812b1f0702eef5ff93933fd0e4c85e08fa65
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-06 22:21:38 +0200

    Add sample code

---
+++ src/NativeMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libpngsharp
+{
+    internal class NativeMethods
+    {
+        private const string Library = "libpng16.dll";
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr png_get_libpng_ver(IntPtr png_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr png_create_read_struct(IntPtr user_png_ver, IntPtr error_ptr, png_error error_fn, png_error warn_fn);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr png_create_info_struct(IntPtr png_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void png_set_read_fn(IntPtr png_ptr, IntPtr io_ptr, [MarshalAs(UnmanagedType.FunctionPtr)]png_rw read_data_fn);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void png_read_info(IntPtr png_ptr, IntPtr info_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint png_get_image_width(IntPtr png_ptr, IntPtr info_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint png_get_image_height(IntPtr png_ptr, IntPtr info_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte png_get_color_type(IntPtr png_ptr, IntPtr info_ptr);
+        
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte png_get_bit_depth(IntPtr png_ptr, IntPtr info_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int png_set_interlace_handling(IntPtr png_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void png_read_update_info(IntPtr png_ptr, IntPtr info_ptr);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void png_read_image(IntPtr png_ptr, IntPtr image);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint png_get_rowbytes(IntPtr png_ptr, IntPtr info_ptr);
+
+        public delegate void png_rw(IntPtr png_ptr, IntPtr outBytes, uint byteCountToRead);
+
+        public delegate void png_error(IntPtr png_structp, IntPtr png_const_charp);
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace libpngsharp
+{
+    public class Png
+    {
+        public Stream Stream
+        {
+            get;
+            set;
+        }
+
+        public void Decode()
+        {
+            // C# equivalent of:
+            // http://zarb.org/~gc/html/libpng.html
+            // http://pulsarengine.com/2009/01/reading-png-images-from-memory/
+            var ver = NativeMethods.png_get_libpng_ver(IntPtr.Zero);
+            var error_fn = new NativeMethods.png_error(OnError);
+            var warn_fn = new NativeMethods.png_error(OnWarning);
+
+            var png_ptr = NativeMethods.png_create_read_struct(ver, new IntPtr(1), error_fn, warn_fn);
+            ThrowOnZero(png_ptr);
+
+            var info_ptr = NativeMethods.png_create_info_struct(png_ptr);
+            ThrowOnZero(info_ptr);
+
+            // Set the callback function
+            var callback = new NativeMethods.png_rw(Read);
+            NativeMethods.png_set_read_fn(png_ptr, IntPtr.Zero, callback);
+
+            NativeMethods.png_read_info(png_ptr, info_ptr);
+
+            var width = NativeMethods.png_get_image_width(png_ptr, info_ptr);
+            var height = NativeMethods.png_get_image_height(png_ptr, info_ptr);
+            var color_type = NativeMethods.png_get_color_type(png_ptr, info_ptr);
+            var bit_depth = NativeMethods.png_get_bit_depth(png_ptr, info_ptr);
+
+            var number_of_passes = NativeMethods.png_set_interlace_handling(png_ptr);
+            NativeMethods.png_read_update_info(png_ptr, info_ptr);
+
+            var bytesPerRow = NativeMethods.png_get_rowbytes(png_ptr, info_ptr);
+            var cb = (int)(bytesPerRow * height);
+
+            IntPtr raw_data = Marshal.AllocHGlobal(cb);
+            IntPtr row_pointers = Marshal.AllocHGlobal((int)Marshal.SizeOf<IntPtr>() * (int)height);
+
+            IntPtr currentRow = raw_data;
+            IntPtr row_pointer = row_pointers;
+
+            for (int i = 0; i < height; i++)
+            {
+                var bytes = BitConverter.GetBytes(currentRow.ToInt64());
+                Marshal.Copy(bytes, 0, row_pointer, Marshal.SizeOf<IntPtr>());
+                currentRow += (int)bytesPerRow;
+                row_pointer += Marshal.SizeOf<IntPtr>();
+            }
+
+            NativeMethods.png_read_image(png_ptr, row_pointers);
+
+            byte[] data = new byte[cb];
+            Marshal.Copy(raw_data, data, 0, cb);
+        }
+
+        private void Read(IntPtr png_ptr, IntPtr outBytes, uint byteCountToRead)
+        {
+            byte[] buffer = new byte[byteCountToRead];
+            this.Stream.Read(buffer, 0, (int)byteCountToRead);
+
+            Marshal.Copy(buffer, 0, outBytes, (int)byteCountToRead);
+        }
+
+        private void OnError(IntPtr png_structp, IntPtr png_const_charp)
+        {
+            var error = Marshal.PtrToStringAnsi(png_const_charp);
+        }
+
+        private void OnWarning(IntPtr png_structp, IntPtr png_const_charp)
+        {
+            var error = Marshal.PtrToStringAnsi(png_const_charp);
+        }
+
+        private void ThrowOnZero(IntPtr value)
+        {
+            if (value == IntPtr.Zero)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
