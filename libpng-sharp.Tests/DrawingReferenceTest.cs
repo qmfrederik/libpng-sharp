25a0bee2068524a2b116573ce0d3a9e09e71310e
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-06-14 14:27:48 +0200

    Remove unused using statement

--- libpng-sharp.Tests/DrawingReferenceTest.cs
+++ libpng-sharp.Tests/DrawingReferenceTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
